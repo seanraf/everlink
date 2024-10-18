6c702098c363a334b9234416c9e31faeaeb8f541
zawarshafiq <zawar.shafiq@devigital.com>
2024-10-18 17:55:52 +0500

    Form Page is complete with Stepper

--- src/view/EverlinkPages.tsx
+++ src/view/EverlinkPages.tsx
@@ -2,21 +2,21 @@
 import React, { useState } from 'react';
 import { Box, Grid2 } from '@mui/material';
 import LinearStepper from '@/components/LinearStepper';
+import Form from './Form';
+import { UrlButton } from '@/types';
 
 export default function EverlinkPages() {
   const [activeStep, setActiveStep] = useState(0);
   const [userName, setUserName] = useState('');
   const [bio, setBio] = useState('');
   const [analyticsTag, setAnalyticsTag] = useState('');
-  const stepFirst = activeStep === 0;
+  const [urlButtons, setUrlButtons] = useState<UrlButton[]>([
+    { id: '1', title: 'Button 1', url: '' },
+  ]);
+
   return (
-    <>
-      <Grid2
-        container
-        mx={'auto'}
-        width={{ md: '85%', xs: '100%' }}
-        bgcolor={'#F9FAFB'}
-      >
+    <Box bgcolor={'#F9FAFB'}>
+      <Grid2 container mx={'auto'} width={{ md: '85%', xs: '100%' }}>
         <Grid2
           size={{ md: activeStep === 0 ? 12 : 7, xs: 12 }}
           mx={activeStep === 0 ? 'auto' : 'unset'}
@@ -31,6 +31,7 @@ export default function EverlinkPages() {
               display: 'flex',
               flexDirection: 'column',
               mx: 'auto',
+              mt: '74px',
               bgcolor: 'primary.contrastText',
               p: { md: '40px 24px', xs: '24px 16px' },
               borderRadius: 3,
@@ -43,7 +44,17 @@ export default function EverlinkPages() {
             </Box>
             {activeStep === 0 ? (
               <>
-                <h1>Form</h1>
+                <Form
+                  userName={userName}
+                  setUserName={setUserName}
+                  bio={bio}
+                  setBio={setBio}
+                  setActiveStep={setActiveStep}
+                  analyticsTag={analyticsTag}
+                  setAnalyticsTag={setAnalyticsTag}
+                  urlButtons={urlButtons}
+                  setUrlButtons={setUrlButtons}
+                />
               </>
             ) : (
               <h1>Select Theme</h1>
@@ -56,12 +67,9 @@ export default function EverlinkPages() {
         >
           <Box>
             <h1>Preview</h1>
-            <h2>{userName}</h2>
-            <h2>{bio}</h2>
-            <h2>{analyticsTag}</h2>
           </Box>
         </Grid2>
       </Grid2>
-    </>
+    </Box>
   );
 }

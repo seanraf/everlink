e2428e6d068ecc21da75acb02c4aac751cd14f13
zawarshafiq <zawar.shafiq@devigital.com>
2024-12-10 22:27:58 +0500

    Display Thanku Page After Payment Successful

--- src/app/failure/page.tsx
+++ src/app/failure/page.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import Link from 'next/link';
 import { Box, Typography } from '@mui/material';
 
 export default function page() {
   return (
     <Box display={'flex'} height={'80vh'}>
-      <Typography sx={{ fontSize: 36, m: 'auto' }}>
-        Payment Not Successful
-      </Typography>
+      <Box m={'auto'} display={'flex'} flexDirection={'column'} gap={2}>
+        <Typography sx={{ fontSize: 30 }}>Payment Not Successful</Typography>
+        <Link
+          href={'/'}
+          style={{
+            textDecoration: 'none',
+            display: 'inline-flex',
+            margin: 'auto',
+          }}
+        >
+          <Typography
+            sx={{
+              p: '3px 8px',
+              border: '2px solid gray',
+              borderRadius: '4px',
+              width: 'fit-content',
+              color: 'secondary.main',
+            }}
+          >
+            Back to Home
+          </Typography>
+        </Link>
+      </Box>
     </Box>
   );
 }

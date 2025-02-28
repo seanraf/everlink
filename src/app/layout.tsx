4691bbd6ba9175b2dcaae8b9f67601fe45dc8040
Usamamian27 <musama@devigital.com>
2025-02-28 17:33:49 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import NavBar from '@/view/NavBar';
 import Providers from '@/providers/provider';
 import CrossmintProvider from '@/providers/Crossmint';
+import FarcasterFrameProvider from '@/providers/FarcasterFrameProvider';
 
 export const metadata: Metadata = {
   title: 'Everlink',
@@ -16,11 +17,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <meta
+          name='fc:frame'
+          content={`{
+            "version": "next",
+            "imageUrl": "https://i.ibb.co/B2V7ddyb/1200-628.png",
+            "button": {
+              "title": "Everlink",
+              "action": {
+                "type": "launch_frame",
+                "name": "Uniframe",
+                "url": "https://create.myeverlink.app",
+                "splashImageUrl": "https://create.myeverlink.app/favicon.ico",
+                "splashBackgroundColor": "#131313"
+              }
+            }
+          }`}
+          data-rh='true'
+        />
+      </head>
       <body suppressHydrationWarning={true}>
         <CrossmintProvider>
           <Providers>
-            <NavBar />
-            {children}
+             <FarcasterFrameProvider>
+              <NavBar />
+              {children}
+            </FarcasterFrameProvider>
           </Providers>
         </CrossmintProvider>
       </body>

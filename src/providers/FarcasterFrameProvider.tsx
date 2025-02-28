c4e46d4fde905b49cf8ee076554d7e94ddeada34
Usamamian27 <musama@devigital.com>
2025-02-28 17:31:11 +0500

    Create FarcasterFrameProvider.tsx

---
+++ src/providers/FarcasterFrameProvider.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { useEffect } from 'react';
+import FrameSDK from '@farcaster/frame-sdk';
+
+function FarcasterFrameProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const load = async () => {
+      FrameSDK.actions.ready();
+    };
+    load();
+  }, []);
+  return <>{children}</>;
+}
+
+export default FarcasterFrameProvider;

7a24efb89f0579bf99b339f886e3fbe1e0f9d5ba
Usamamian27 <musama@devigital.com>
2025-06-17 14:06:22 +0500

    Update Crossmint.tsx

--- src/providers/Crossmint.tsx
+++ src/providers/Crossmint.tsx
@@ -9,11 +9,6 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CrossmintProvider apiKey={process.env.NEXT_PUBLIC_CROSSMINT_API_KEY ?? ''}>
       <CrossmintAuthProvider
-{/*         embeddedWallets={{
-          type: 'evm-smart-wallet',
-          defaultChain: 'base-sepolia',
-          createOnLogin: 'all-users',
-        }} */}
         loginMethods={['farcaster']}
       >
         {children}
